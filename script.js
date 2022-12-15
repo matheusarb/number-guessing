d8ea2114860520bb91c5c585a9b5c89a55f6d8a8
Matheus <mat.araujoribeiro@gmail.com>
2022-12-15 15:47:40 -0300

    adding DOM manipulation with Vanila JS

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 "use strict";
 
 console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number 😎";
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 11;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 9;
