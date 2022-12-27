a8310a88d1d5d30a2b737cbdd024f0bd91a6ff04
Matheus <mat.araujoribeiro@gmail.com>
2022-12-27 11:10:27 -0300

    add highscore logic

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ document.querySelector(".guess").value = 23;
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -63,10 +64,13 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent =
       "Congratulations! The guess is correct!! 🎉";
     document.querySelector(".number").textContent = secretNumber;
-
     document.querySelector("body").style.backgroundColor = "#60b347";
-
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   }
 });
 
